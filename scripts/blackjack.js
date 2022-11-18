eba4e703c2f078cea94e9d05437c0fd27a0e0007
FlorianR <52248349+MonkeyWaffle0@users.noreply.github.com>
2022-11-18 17:31:10 -0500

    dealer, deck and player logic in separate files
    * dealer, deck and player logic in separate files
    
    * x
    
    Co-authored-by: Florian Rea <florian.rea@dental-wings.com>
    Co-authored-by: Florian-grazina <floriangrazina@gmail.com>

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,98 +1,58 @@
 // Game setup
-var betChips = 0;
-var preBet = 0;
-var bet = 0;
-var result = "Place your bet!"
+import { TimeUtils } from "./utils/time-utils.js";
+import { ButtonUtils } from "./utils/button-utils.js";
+import { Deck } from "./models/deck.js";
+import { Dealer } from "./models/dealer.js";
+import { Player } from "./models/player.js";
 
-var chips = 1000;
-var stack = ""
-var totalPlayer = 0;
-var playerAce = 0;
-var playerBj = false;
+let betChips = 0;
+let preBet = 0;
+let bet = 0;
+let result = "Place your bet!"
 
-var totalDealer = 0;
-var dealerAce = 0;
-var dealerBj = false;
+let chips = 1000;
 
-dealer("Idle");
+Dealer.drawIdleFrame();
 drawCards();
 show();
 
-// Setup deck of cards
-const color = ["Hearts","Spades","Diamond","Clubs"]
-const cardId = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-const cardValue = [11,2,3,4,5,6,7,8,9,10,10,10,10];
-const cardName = ["Ace","Two", "Tree", "Four", "Five", "Six",
-"Seven","Eight","Nine","Ten","Jack","Queen","King"];
-
-var cards = cardId.concat(cardId).concat(cardId).concat(cardId);
-var deck = cards.concat(cards).concat(cards).concat(cards)
-
-
 //  Pick up buttons in HTML
-var btnChips = document.getElementById("btnChips");
+const btnChips = document.getElementById("btnChips");
 btnChips.addEventListener ("click", betChipsClick);
 
-var btnBet = document.getElementById("btnBet");
+const btnBet = document.getElementById("btnBet");
 btnBet.addEventListener ("click", betCheck);
 
-var btnHit = document.getElementById("btnHit");
+const btnHit = document.getElementById("btnHit");
 btnHit.addEventListener ("click", hit);
-btnDisable(btnHit);
+ButtonUtils.btnDisable(btnHit);
 
-var btnStand = document.getElementById("btnStand");
+const btnStand = document.getElementById("btnStand");
 btnStand.addEventListener ("click", stand);
-btnDisable(btnStand);
+ButtonUtils.btnDisable(btnStand);
 
-var btnDouble = document.getElementById("btnDouble");
+const btnDouble = document.getElementById("btnDouble");
 btnDouble.addEventListener ("click", double);
-btnDisable(btnDouble);
+ButtonUtils.btnDisable(btnDouble);
 
 // Shortcuts keyboard
 document.onkeydown = function(e){
     switch(e.keyCode) {
         case 72:
-            if (btnHit.disabled == false){
-                hit();
-            } else {
-                return;
-            }
+            if (!btnHit.disabled) hit();
             break;
         case 83:
-            if (btnStand.disabled == false){
-                stand();
-            } else {
-                return;
-            }
+            if (!btnStand.disabled) stand();
             break;
         case 68:
-            if (btnDouble.disabled == false){
-                double();
-            } else {
-                return;
-            }
+            if (!btnDouble.disabled) double();
             break;
         case 13:
             e.preventDefault()
-            if (btnBet.disabled == false){
-                betCheck();
-            } else {
-                return;
-            }
+            if (!btnBet.disabled) betCheck();
             break;
     }
 }
-
-
-// Sleep
-function sleep(wait){
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('resolved');
-        },wait);
-    });
-}
-
 
 // Click bet
 function betChipsClick(e){
@@ -102,14 +62,14 @@ function betChipsClick(e){
         return;
     }
 
-    if (betChips == 0){         //le bouton reset a une valeur 0
+    if (betChips === 0){         //le bouton reset a une valeur 0
         preBet = 0;
         for (let i = 0; i < 4; i++){
             document.getElementById("stack"+i).innerHTML="";
         }
     }
     else{     //Si on appuye sur un bouton autre que reset
-        if (preBet == chips){    //Si le joueur à déja la mise maxi
+        if (preBet === chips){    //Si le joueur à déja la mise maxi
             return
         }
         else if (preBet+betChips > chips){     //Si le joueur veut trop miser
@@ -119,8 +79,8 @@ function betChipsClick(e){
             preBet += betChips;
         }
 
-        i = Math.floor(Math.random()*4)
-        image = document.createElement("img");
+        let i = Math.floor(Math.random()*4)
+        let image = document.createElement("img");
         image.src = "./Sprites/"+betChips+" Stack.png";
         image.className = "stack";
         document.getElementById("stack"+i).appendChild(image);
@@ -148,10 +108,9 @@ function betCheck() {
 
 // Start the game
 async function startGame(){
-    btnDisable(btnBet);
+    ButtonUtils.btnDisable(btnBet);
 
     result = "No more bets";
-    action = "...";
     chips -= bet;
 
     show();
@@ -162,18 +121,19 @@ async function startGame(){
     await drawCardPlayer();
     document.getElementById("dealerCards").innerHTML = "";
     await drawCardDealer();
+    show();
     await drawCardPlayer();
 
-    btnEnable(btnStand);
-    btnEnable(btnHit);
-    btnEnable(btnDouble)
+    ButtonUtils.btnEnable(btnStand);
+    ButtonUtils.btnEnable(btnHit);
+    ButtonUtils.btnEnable(btnDouble)
 
     document.getElementById("result").style.display = "none";
 
-    if (totalPlayer == 21){
+    if (totalPlayer === 21){
         playerBj = true;
-        btnDisable(btnHit);
-        btnDisable(btnDouble),
+        ButtonUtils.btnDisable(btnHit);
+        ButtonUtils.btnDisable(btnDouble);
         result = "Blackjack!"
         document.getElementById("result").style.display = "block";
         show();
@@ -181,111 +141,64 @@ async function startGame(){
 }
 
 
-// Draw player
 async function drawCardPlayer() {
-    let i = Math.floor(Math.random()*deck.length);
-    let x = deck[i]-1;
-    totalPlayer += cardValue[x];
-    deck.splice (i,1);
-    if (cardId[x] == 1){
-        playerAce++;
-    }
-
-    if (totalPlayer > 21 && playerAce > 0){
-    totalPlayer -= 10;
-    playerAce--;
-    }
-
-    i = Math.floor(Math.random()*4); // Color random
-
-    
-    image = document.createElement("img"); // Cards show
-    image.src = "./Sprites/Cards/"+color[i]+" "+cardId[x]+".png";
-    image.className="cards";
-
-    if (document.getElementById("playerCards").length == 0){
-        document.getElementById("playerCards") = image;
-    } else{
-        document.getElementById("playerCards").appendChild(image);
-    }
-
+    Player.drawCard();
     show();
-    await dealerAnimation();
+    await Dealer.playAnimation();
 }
 
-
-// Draw dealer
 async function drawCardDealer() {
-
-    let i = Math.floor(Math.random()*deck.length);
-    let x = deck[i]-1;
-    totalDealer += cardValue[x];
-    deck.splice (i,1);
-    if (cardId[x] == 1){
-        dealerAce++;
-    }
-
-    if (totalDealer > 21 && dealerAce > 0){
-    totalDealer -= 10;
-    dealerAce--;
-    }
-
-    i = Math.floor(Math.random()*4); // Color random
-
-    image = document.createElement("img"); // Cards show
-    image.src = "./Sprites/Cards/"+color[i]+" "+cardId[x]+".png";
-    image.className="cards";
-    if (document.getElementById("dealerCards").length == 0){
-        document.getElementById("dealerCards") = image;
-    } else{
-        document.getElementById("dealerCards").appendChild(image);
-    }
-
+    await Dealer.drawCard();
     show();
-    await dealerAnimation();
+    await Dealer.playAnimation();
 }
 
 
 async function hit() {
-    action = "Hit"
-    drawCardPlayer();
-    // show();
-    btnDisable(btnDouble);
-    if (totalPlayer > 21){
+    Player.drawCard();
+    show();
+    ButtonUtils.btnDisable(btnDouble);
+    if (Player.getTotal() > 21){
         result = "Too many";
-        btnDisable(btnHit);
-        btnDisable(btnDouble);
-        btnDisable(btnStand);
+        ButtonUtils.btnDisable(btnHit);
+        ButtonUtils.btnDisable(btnDouble);
+        ButtonUtils.btnDisable(btnStand);
+        await Dealer.playAnimation();
         document.getElementById("result").style.display = "block";
         show();
-        await sleep(2000);
+        await TimeUtils.sleep(2000);
         reset();
         show();
-    } 
+    } else {
+        await Dealer.playAnimation();
+    }
+
 }
 
 
 async function stand(){
     document.getElementById("result").style.display = "none";
-    action = "Stand"
-    btnDisable(btnHit);
-    btnDisable(btnDouble);
-    btnDisable(btnStand);
+    ButtonUtils.btnDisable(btnHit);
+    ButtonUtils.btnDisable(btnDouble);
+    ButtonUtils.btnDisable(btnStand);
 
-    while (totalDealer < 17){
+    while (Dealer.getTotal() < 17){
         await drawCardDealer();
     }
 
-    if (playerBj == true && dealerBj === false){
+    const dealerTotal = Dealer.getTotal();
+    const playerTotal = Player.getTotal();
+
+    if (Player.hasBlackJack() && !Dealer.hasBlackJack()){
         chips += (bet*2.5);
-        result = "You won! : "+bet*1.5+" chips.";
+        result = "You won! : " + bet * 1.5 + " chips.";
 
-    } else if (totalDealer > 21 || totalDealer < totalPlayer){
+    } else if (dealerTotal > 21 || dealerTotal < playerTotal){
         chips += (bet*2);
-        result = "You won! : "+bet+" chips.";
+        result = "You won! : " + bet + " chips.";
 
-    } else if (totalDealer > totalPlayer){
-        result = "You lost : "+bet+" chips.";
+    } else if (dealerTotal > playerTotal){
+        result = "You lost : " + bet + " chips.";
 
     } else {
         result = "Draw";
@@ -293,67 +206,52 @@ async function stand(){
     }
     document.getElementById("result").style.display = "block";
     show();
-    await sleep(2000);
+    await TimeUtils.sleep(2000);
     reset();
     show();
 }
 
 
-function double(){
-    if (chips < bet){
+async function double() {
+    if (chips < bet) {
         window.alert("You don't have enough chips!")
-    }
-    else{
-        action = "Double";
+    } else {
         chips -= bet;
-        bet = bet*2;
-        drawCardPlayer();
+        bet = bet * 2;
+        await drawCardPlayer();
         show();
-        btnDisable(btnDouble);
+        ButtonUtils.btnDisable(btnDouble);
 
-        if (totalPlayer > 21){
-            result = "Too many"
+        if (Player.getTotal() > 21) {
+            result = "Too many";
             reset();
         }
-
-        btnDisable(btnHit);
-        btnDisable(btnDouble);
+        ButtonUtils.btnDisable(btnHit);
+        ButtonUtils.btnDisable(btnDouble);
     }
 }
 
 
 function show() {
-    if (playerAce > 0){
-        document.getElementById("totalPlayer").innerHTML = totalPlayer + " (Soft)";
-    } else {
-        document.getElementById("totalPlayer").innerHTML = totalPlayer;
-    }
+    const playerTotal = Player.getTotal();
+    document.getElementById("totalPlayer").innerHTML = Player.getAce() > 0 ? playerTotal + " (Soft)" : playerTotal;
     document.getElementById("chips").innerHTML = chips;
-    document.getElementById("totalDealer").innerHTML = totalDealer;
+    document.getElementById("totalDealer").innerHTML = Dealer.getTotal();
     document.getElementById("betAmount").innerHTML = preBet;
     document.getElementById("result").innerHTML = result;
-    
 }
 
 
 function reset(){
-
-    btnEnable(btnBet);
-    btnDisable(btnStand);
-    btnDisable(btnHit);
-    btnDisable(btnDouble);
-    totalPlayer = 0;
-    totalDealer = 0;
-    playerAce = 0;
-    dealerAce = 0;
-    playerBj = false;
-    dealerBj = false;
+    ButtonUtils.btnEnable(btnBet);
+    ButtonUtils.btnDisable(btnStand);
+    ButtonUtils.btnDisable(btnHit);
+    ButtonUtils.btnDisable(btnDouble);
     result = "Place your bet!";
     drawCards();
-    if (deck.length < cards.length*2){
-        deck = cards.concat(cards).concat(cards).concat(cards);
-        action = 'Reshuffling';
-    }
+    Deck.reset();
+    Dealer.reset();
+    Player.reset();
 }
 
 
@@ -361,44 +259,18 @@ function reset(){
 function drawCards(){
 
     document.getElementById("dealerCards").innerHTML = "";
-    image = document.createElement("img"); // Cards show
+    let image = document.createElement("img"); // Cards show
     image.className="cards";
-    image.src = "./Sprites/Cards/Back Grey 1.png";
+    image.src = Deck.greyCardPath;
     image.value = 0;
     document.getElementById("dealerCards").appendChild(image);
     
     document.getElementById("playerCards").innerHTML = "";
     image = document.createElement("img"); // Cards show
     image.className="cards";
-    image.src = "./Sprites/Cards/Back Grey 1.png";
+    image.src = Deck.greyCardPath;
     image.value = 0;
     document.getElementById("playerCards").appendChild(image);
-}
-
-
-//Draw dealer
-function dealer(a){
-    var dealer = document.getElementById("dealerSprite");
-    dealer.src = "./Sprites/"+a+".png";
-    document.getElementById("dealerSprite").innerHTML = dealerSprite;
-}
-
-async function dealerAnimation(){
-    dealer("Draw");
-    await sleep(300);
-    dealer("Idle");
-    await sleep(300);
-}
-
-//Style
-function btnDisable(btn){
-    btn.disabled = true;
-    btn.className = "buttonGray";
-}
-
-function btnEnable(btn){
-    btn.disabled = false;
-    btn.className = "button";
 }
 
 //Functions
